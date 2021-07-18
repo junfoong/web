901ba95eeea2972f3ee12be021ee8722dd27ce0f
joo hang <joohangooi@gmail.com>
2021-07-18 20:45:03 +0800

    'First'

---
+++ Index.php
@@ -0,0 +1,91 @@
+<html>
+<head>
+    <link href="index.css" rel="stylesheet" type="text/css"/>
+    <script src="index.js" defer></script>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Index</title>
+</head>
+<!--command-->
+<header>
+    <div class="navContainer" id="navbar">
+        <div>
+            <nav>
+                <ul>
+                    <li><a href="#">Nav1</a></li>
+                    <li><a href="#">Nav1</a>
+                        <!--First drop down list-->
+                        <ul>
+                            <li><a href="#">Nav2</a></li>
+                            <li><a href="#">Nav2</a></li>
+                            <li><a href="#">Nav2</a></li>
+                        </ul>
+                    </li>
+                    <!--First drop down list end-->
+
+                    <li><a href="#">Nav1</a>
+                        <!--First drop down list Start-->
+                        <ul>
+                            <li><a href="#">Nav2</a></li>
+                            <li><a href="#">Nav2</a></li>
+                            <li><a href="#">Nav2</a>
+                                <ul>
+                                    <li><a href="#">Nav3</a></li>
+                                    <li><a href="#">Nav3</a></li>
+                                    <li><a href="#">Nav3</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </li>
+                    <li><a href="#">Nav1</a></li>
+                    <li><a href="#">Nav1</a></li>
+                    <span id="signIn">
+                    <li><a href="#">Sign Up</a> </li>
+                    <li><a href="#">Log In</a> </li>
+                </span>
+                </ul>
+            </nav>
+        </div>
+        <div class="slideContainer">
+            <img class="mySlides" src="Image/Slide-1.jpg" style="width:100%;height: 80%">
+            <img class="mySlides" src="Image/Slide-2.jpg" style="width:100%;height: 80%">
+            <img class="mySlides" src="Image/Slide-3.jpg" style="width:100%;height: 80%">
+        </div>
+</header>
+<body>
+    <h1>Recent Activity</h1>
+        <div class="wrapper">
+    <section class="columns">
+        <div class="column">
+            <h2>1st Content Area</h2>
+            <p>Every option should be explored and tried no matter how unpredictable, impossible, insane or ridiculous does it looks or sound.</p>
+            <button class="button" type="button" role="button" value="MORE">More</button>
+        </div>
+
+        <div class="column">
+            <h2>2nd Content Area</h2>
+            <p>Every option should be explored and tried no matter how unpredictable, impossible, insane or ridiculous does it looks or sound.</p>
+
+            <button class="button" type="button" role="button" value="MORE">More</button>
+        </div>
+
+        <div class="column">
+            <h2>3rd Content Area</h2>
+            <p>Every option should be explored and tried no matter how unpredictable, impossible, insane or ridiculous does it looks or sound.</p>
+            <button class="button" type="button" role="button" value="MORE">More</button>
+        </div>
+    </section>
+
+</div>
+
+
+</body>
+<style>
+
+
+</style>
+
+
+</html>
